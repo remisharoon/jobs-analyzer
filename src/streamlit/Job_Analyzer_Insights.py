52616b716a29c26d63b9997dccaf76e055557d4e
remisharoon <remisharoon@gmail.com>
2024-03-24 12:34:23 +0400

    adding more vizs

--- src/streamlit/Job_Analyzer_Insights.py
+++ src/streamlit/Job_Analyzer_Insights.py
@@ -1,13 +1,37 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
+from jobs_data import load_dataframe
+import sys
+import os
 
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
 
-df
+st.set_page_config(
+    page_title="Job Listings Dashboard",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://www.linkedin.com/in/remisharoon/',
+        # 'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an *extremely* cool app!"
+    }
+)
+
+
 
 st.write("Hello Jobs Analyzer")
 
+df = load_dataframe()
+
+location = st.sidebar.multiselect('Location', options=df['location'].unique())
+if location:
+    df_filtered = df[df['location'].isin(location)]
+else:
+    df_filtered = df
+
+st.write(df_filtered)
+
+
+df
---
+++ src/streamlit/jobs_data.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+import streamlit as st
+import pandas as pd
+
+connection_string = st.secrets.PostgresDB.connection_string
+conn = create_engine(connection_string)
+
+# @st.cache(allow_output_mutation=True)
+@st.cache_data
+def load_dataframe(sql='select * from ja_jobs_raw'):
+    SQL_Query = pd.read_sql(sql, conn)
+    df = pd.DataFrame(SQL_Query)
+    df['date_posted'] = pd.to_datetime(df['date_posted'],errors='coerce')  # Use 'coerce' to handle any invalid parsing as NaT
+    df = df.dropna(subset=['date_posted'])  # Optional: Remove rows where conversion failed
+    return df
