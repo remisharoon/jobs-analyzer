679ab62e24c8dec14dc578aaeb753304d4d81d9c
remisharoon <remisharoon@gmail.com>
2024-03-30 17:52:20 +0400

    standardize tech jobs names

--- src/streamlit/jobs_data.py
+++ src/streamlit/jobs_data.py
@@ -5,7 +5,8 @@ import pandas as pd
 connection_string = st.secrets.PostgresDB.connection_string
 conn = create_engine(connection_string)
 
-sqlQuery = """SELECT 
+sqlQuery = """
+				SELECT 
                   site, 
                   job_url, 
                   job_url_direct, 
@@ -32,11 +33,11 @@ sqlQuery = """SELECT
                   banner_photo_url, 
                   ceo_name, 
                   ceo_photo_url, 
-                  job_hash, 
+                  jr.job_hash, 
                   jcm.target_value as country_inferred, 
                   state_inferred, 
                   city_inferred, 
-                  desired_tech_skills_inferred, 
+                  tss.desired_tech_skills_inferred, 
                   desired_soft_skills_inferred, 
                   desired_domain_skills_inferred, 
                   domains_inferred, 
@@ -60,7 +61,11 @@ sqlQuery = """SELECT
                   "company industry" 
                 FROM 
                   ja_jobs_raw jr 
-                  left outer join ja_country_names_std_mapping jcm on jr.country_inferred = jcm.source_value;
+                left outer join ja_country_names_std_mapping jcm on jr.country_inferred = jcm.source_value
+                left outer join (SELECT job_hash, string_agg(desired_tech_skill_standardized, ',') as desired_tech_skills_inferred
+								FROM ja_job_tech_skills
+								where desired_tech_skill_standardized is not null and desired_tech_skill_standardized != 'TO_BE_DECIDED'
+								GROUP BY job_hash) tss on jr.job_hash = tss.job_hash;
                 """
 
 
